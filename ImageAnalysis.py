4aeb65cf2fbe3354aecd97bd676cf0ad036db2f3
Benjamin Frost <ben.mark.frost@gmail.com>
2021-12-03 13:42:30 +0000

    Adding adaptive movement speed

--- ImageAnalysis.py
+++ ImageAnalysis.py
@@ -3,6 +3,9 @@ import numpy as np
 import threading
 import os
 import time
+from object_detector import ObjectDetector
+from object_detector import ObjectDetectorOptions
+import utils
 
 class ImageAnalysis:
     
@@ -12,14 +15,21 @@ class ImageAnalysis:
         time.sleep(1)
         
         self.position = "none"
+        self.mid_x = 0
         self.faceSize = 1000000000
 
         self.camera = cv2.VideoCapture(0)
-        self.camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-        self.camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+        self.camera.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        self.camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
         
         self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-        #self.detector = MTCNN()
+        
+        options = ObjectDetectorOptions(
+          num_threads=2,
+          score_threshold=0.3,
+          max_results=3,
+          enable_edgetpu=False)
+        self.detector = ObjectDetector(model_path="efficientdet_lite0.tflite", options=options)
 
         self.thread = threading.Thread(target=self.start)
         self.thread.start()
@@ -43,6 +53,76 @@ class ImageAnalysis:
         img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
         return img
 
+
+    def detectFaces(self, frame, leftBound, rightBound):
+        gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        
+        faces = self.face_cascade.detectMultiScale(gray, 1.1, 4)
+
+        #faces = self.detector.detect_faces(frame)
+
+        #faces = [el['box'] for el in faces]
+
+
+        biggestFace = [0, (0,0), (0,0)]
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            if w*h > biggestFace[0]:
+                biggestFace[0] = w*h
+                biggestFace[1] = (x, y)
+                biggestFace[2] = (w, h)
+        
+        if biggestFace[0] > 1000:
+                
+            self.faceSize = biggestFace[0]
+            
+            cv2.rectangle(frame, biggestFace[1], (biggestFace[1][0] + biggestFace[2][0], biggestFace[1][1] + biggestFace[2][1]), (0, 255, 0), 2)
+
+            if leftBound > (biggestFace[1][0] + (biggestFace[2][0]/2)):
+                #print("Left" + np.random.randint(0, 100).__str__())
+                self.position = "left"
+            elif rightBound < (biggestFace[1][0] + (biggestFace[2][0]/2)):
+                #print("Right" + np.random.randint(0, 100).__str__())
+                self.position = "right"
+            else:
+                self.position = "center"
+        else:
+            self.postion = "none"
+        
+        
+
+    def detectPeople(self, frame, leftBound, rightBound):
+        detections = self.detector.detect(frame)
+            
+        frame = utils.visualize(frame, detections)
+        
+        #print(detections)
+        
+        for detection in detections:
+            if detection.categories[0].label == "person":
+                #print("Found person")
+                #print(detection.bounding_box)
+                bounding_box = list(detection.bounding_box)
+                #print(bounding_box)
+                
+                
+                self.mid_x = bounding_box[0] + ((bounding_box[2] - bounding_box[0])/2)
+                
+                
+                if leftBound > (self.mid_x):
+                    #print("Left" + np.random.randint(0, 100).__str__())
+                    self.position = "left"
+                elif rightBound < (self.mid_x):
+                    #print("Right" + np.random.randint(0, 100).__str__())
+                    self.position = "right"
+                else:
+                    self.position = "center"
+                
+                return
+        self.postion = "none"
+            
+
     def start(self):
         
         while True:
@@ -52,49 +132,19 @@ class ImageAnalysis:
 
             # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-            gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-            
-
-            faces = self.face_cascade.detectMultiScale(gray, 1.1, 4)
-
-            #faces = self.detector.detect_faces(frame)
-
-            #faces = [el['box'] for el in faces]
-
-
-            biggestFace = [0, (0,0), (0,0)]
-
-            for (x, y, w, h) in faces:
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-                if w*h > biggestFace[0]:
-                    biggestFace[0] = w*h
-                    biggestFace[1] = (x, y)
-                    biggestFace[2] = (w, h)
             
             
-            leftBound = int(((frame.shape[1]/2) - frame.shape[1]/20))
-            rightBound = int(((frame.shape[1]/2) + frame.shape[1]/20))
+            
+            self.leftBound = int(((frame.shape[1]/2) - frame.shape[1]/20))
+            self.rightBound = int(((frame.shape[1]/2) + frame.shape[1]/20))
 
-            cv2.rectangle(frame, (leftBound, -10), (rightBound, int(frame.shape[0]+10)), (0, 0, 255), 2)
+            cv2.rectangle(frame, (self.leftBound, -10), (self.rightBound, int(frame.shape[0]+10)), (0, 0, 255), 2)
 
             
-
-            if biggestFace[0] > 1000:
-                
-                self.faceSize = biggestFace[0]
-                
-                cv2.rectangle(frame, biggestFace[1], (biggestFace[1][0] + biggestFace[2][0], biggestFace[1][1] + biggestFace[2][1]), (0, 255, 0), 2)
-
-                if leftBound > (biggestFace[1][0] + (biggestFace[2][0]/2)):
-                    #print("Left" + np.random.randint(0, 100).__str__())
-                    self.position = "left"
-                elif rightBound < (biggestFace[1][0] + (biggestFace[2][0]/2)):
-                    #print("Right" + np.random.randint(0, 100).__str__())
-                    self.position = "right"
-                else:
-                    self.position = "center"
-            else:
-                self.postion = "none"
+            #self.detectFaces(frame, leftBound, rightBound)
+            
+            self.detectPeople(frame, self.leftBound, self.rightBound)
+            
 
             cv2.putText(frame, self.position, (25, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
             cv2.imshow('frame', frame)
--- start.py
+++ start.py
@@ -3,6 +3,8 @@ import time
 import ImageAnalysis
 
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=5)
+tempMove = ""
+counter = 5
 
 input_string = ser.readline().decode("utf-8").strip()
 if input_string:
@@ -13,23 +15,60 @@ def send(message, moveTime):
     formattedMessage = (f"%s %s" % (message, moveTime))
     ser.write((formattedMessage+"\n").encode())
     input_string = ser.readline().decode("utf-8").strip()
-    print(input_string)
+    #print(input_string)
     return input_string
     
     
 imageAnalysis = ImageAnalysis.ImageAnalysis()
+
+def move(direction, moveTime):
+    
+    if direction == "right":
+        send("rgt", moveTime)
+    elif direction == "left":
+        send("lft", moveTime)
+    
+    time.sleep((moveTime)/1000)
+    
 
 while True:
 
     result = imageAnalysis.getPosition()
     faceSize = imageAnalysis.getFaceSize()
     #print(result)
-    moveTime = 20
-
+    moveTime = 10
+    
+    mid_x = imageAnalysis.mid_x
+    #print(mid_x)
+    
+    
+        
+    if (result == "none") & (counter > 0):
+            
+        if (tempMove == "right" or tempMove == "left"):
+            
+            print("should keep moving", tempMove, counter)
+            
+            move(tempMove, moveTime)
+                
+            counter -= 1
+    
+        continue
+    
+    distance = 0
     if result == "right":
-        send("rgt", moveTime)
+        distance = mid_x - imageAnalysis.rightBound
     elif result == "left":
-        send("lft", moveTime)
+        distance = imageAnalysis.leftBound - mid_x
+    
+    
+    moveTime = int((moveTime * distance/30))
+    print(moveTime)
+    
+
+    move(result, moveTime)
+    tempMove = result
+    counter = 2
         
     #if faceSize < 60000:
         #send("fwd",40)
